c65d263f21fdbd2d985d332e5314766b33e345ad
Manuel Ottlik <manuel.ottlik@web.de>
2020-03-21 00:18:14 +0100

    add project

---
+++ api.js
@@ -0,0 +1,86 @@
+let myId = 1;
+
+const NProgressWrapper = {
+    pendingRequests: [],
+
+    maxAmount: 0,
+
+    getProgress() {
+        return (0.3 + this.maxAmount - this.pendingRequests.length) / this.maxAmount;
+    },
+
+    addRequest(request) {
+        if (this.pendingRequests.length > 0) {
+            this.maxAmount++;
+            NProgress.set(this.getProgress());
+        } else {
+            this.maxAmount = 1;
+            NProgress.start();
+        }
+
+        this.pendingRequests.push(request);
+    },
+
+    removeRequest(request) {
+        const requestIndex = this.pendingRequests.findIndex(item => item._myId == request._myId);
+
+        this.pendingRequests.splice(requestIndex, 1);
+
+        if (this.pendingRequests.length > 0) {
+            NProgress.set(this.getProgress());
+        } else {
+            this.maxAmount = 0;
+            NProgress.done();
+        }
+    }
+}
+
+const onRequestSuccess = (request) => {
+    request._myId = myId++;
+
+    //NProgressWrapper.addRequest(request);
+    NProgress.start();
+
+    return request;
+};
+
+const onRequestError = null;
+
+const onResponseSuccess = (response) => {
+    //NProgressWrapper.removeRequest(response.config);
+    NProgress.done();
+
+    return response;
+};
+
+const onResponseError = (error) => {
+    //NProgressWrapper.removeRequest(error.response.config);
+    NProgress.done();
+
+    return error;
+};
+
+const api = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com/'
+});
+
+api.interceptors.request.use(onRequestSuccess, onRequestError);
+api.interceptors.response.use(onResponseSuccess, onResponseError);
+
+const getPosts = async () => {
+    const postsRequest = await api.get('/posts');
+
+    posts = postsRequest.data.map(async item => {
+        const post = item;
+
+        const userRequest = await api.get(`/users/${post.userId}`);
+        const commentsRequest = await api.get(`/posts/${post.id}/comments`);
+
+        post.user = userRequest.data;
+        post.comments = commentsRequest.data;
+
+        return post;
+    });
+
+    return Promise.all(posts);
+}
---
+++ main.js
@@ -0,0 +1,43 @@
+function htmlToElement(html) {
+    var template = document.createElement('template');
+    html = html.trim();
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
+
+const loadContent = async () => {
+    const posts = await getPosts();
+
+    const contentWrapper = document.getElementById("content");
+
+    posts.forEach(post => {
+        //console.log(post)
+
+        let commentsMarkup = '';
+
+        post.comments.forEach(comment => {
+            commentsMarkup = commentsMarkup + `<br><div><b>${comment.email}:</b> ${comment.name}</div>`;
+        });
+
+        const postMarkup = `
+                <div class="card">
+                    <div class='card-header'><b>${post.title}</b></div>
+                    <div class="card-body">                        
+                        <p class="card-text">${post.body}</p>
+                        <hr>
+                        <h5>Kommentare</h5>
+                        ${commentsMarkup}
+                    </div>
+
+                    <div class='card-footer'>
+                        <div class='text-right text-muted'>verfasst von ${post.user.name}</div>
+                    </div>
+                </div>
+
+            `
+
+        contentWrapper.appendChild(htmlToElement(postMarkup));
+    })
+};
+
+window.onload = loadContent;
